d09d6bde48cdc4e600687ec274a51892c35493cb
Plyxen <kucsak.akos@gmail.com>
2025-11-04 15:16:05 +0100

    Implement character counting function in egyedi_betuk

--- feladatok.py
+++ feladatok.py
@@ -1,4 +1,26 @@
 def egyedi_betuk(szoveg:str=""):
-    # ide kell írni a megoldást ciklussal és elágazással
-
-    return []
+    betuk = ""
+    i = 0
+    
+    while i < len(szoveg):
+        if szoveg[i].isalpha():
+            betu = szoveg[i].lower()
+            if betu not in betuk:
+                betuk += betu
+        i += 1
+    
+    betuk = "".join(sorted(betuk))
+    eredmeny = []
+    j = 0
+    
+    while j < len(betuk):
+        szamlalas = 0
+        k = 0
+        while k < len(szoveg):
+            if szoveg[k].isalpha() and szoveg[k].lower() == betuk[j]:
+                szamlalas += 1
+            k += 1
+        eredmeny.append(betuk[j] + ":" + str(szamlalas))
+        j += 1
+    
+    return eredmeny
